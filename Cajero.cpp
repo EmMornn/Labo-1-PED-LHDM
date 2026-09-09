b2520b14b4f64bebd7a205aaaa31189c7854f034
EmMornn <00000426>
2026-09-09 14:19:54 -0600

    feature/menu

--- Cajero.cpp
+++ Cajero.cpp
@@ -1,2 +1,13 @@
 #include <iostream>
 using namespace std;
+
+void MostrarMenu()
+{
+    cout << endl;
+    cout << "CAJERO POR LHDM" << endl;
+    cout << "1. Ver Saldo Disponible" << endl;
+    cout << "2. Transferir Dinero" << endl;
+    cout << "3. Retirar Dinero (en multiplos de 5)" << endl;
+    cout << "4. Salir" << endl;
+    cout << endl;
+};
